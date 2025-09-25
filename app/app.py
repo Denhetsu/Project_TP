7823d9ac89d0ea977861c3529f717b8f6c81884f
Leo Milovanovic <leomilo719@gmail.com>
2025-09-25 20:14:39 +0200

    rollback to nginx

--- app/app.py
+++ app/app.py
@@ -1,10 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello, Git project is working!"
+def main():
+    print("Hello, Git project is working!")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    main()
